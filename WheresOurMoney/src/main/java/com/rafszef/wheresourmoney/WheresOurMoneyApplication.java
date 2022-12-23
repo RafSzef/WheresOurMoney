36343a74095290caee0ef55c4c91d848919b99ea
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2022-12-23 20:24:01 +0100

    Main config + database connection

---
+++ WheresOurMoney/src/main/java/com/rafszef/wheresourmoney/WheresOurMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.rafszef.wheresourmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheresOurMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheresOurMoneyApplication.class, args);
+    }
+
+}
---
+++ WheresOurMoney/src/test/java/com/rafszef/wheresourmoney/WheresOurMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafszef.wheresourmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheresOurMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
