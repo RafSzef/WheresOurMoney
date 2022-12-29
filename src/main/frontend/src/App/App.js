06f68030df2dafaf42ab674b88f2d03ebc6702c8
Rafał Szefler <59801632+RafSzef@users.noreply.github.com>
2022-12-29 15:25:20 +0100

    routing added

--- src/main/frontend/src/App/App.js
+++ src/main/frontend/src/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from "../compontents/Navbar";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import EntryTable from "../compontents/EntryTable";
 import UsersTable from "../compontents/UsersTable";
@@ -7,14 +8,26 @@ import CategoryTable from "../compontents/CategoryTable";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar></Navbar>
-            {/*<EntryTable></EntryTable>*/}
-            {/*<UsersTable></UsersTable>*/}
-            <CategoryTable></CategoryTable>
-            <header className="App-header">
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Switch>
+                    <Route path="/users">
+                        <UsersTable />
+                    </Route>
+                    <Route path="/categories">
+                        <CategoryTable />
+                    </Route>
+                    <Route path="/entries">
+                        <EntryTable/>
+                    </Route>
+                </Switch>
+                {/*<EntryTable></EntryTable>*/}
+                {/*<UsersTable></UsersTable>*/}
+                {/*<CategoryTable></CategoryTable>*/}
+
+            </div>
+        </Router>
     );
 }
 
--- src/main/frontend/src/compontents/Navbar.js
+++ src/main/frontend/src/compontents/Navbar.js
@@ -1,16 +1,29 @@
 import React from "react";
 import logo from "../logo.svg";
+import {Link} from "react-router-dom";
+
 
 export default function Navbar() {
-    return (
-        <nav>
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h3 className="nav--logo_text">WheresOurMoney</h3>
-            <ul>
-                <li>Accounts</li>
-                <li>Categories</li>
-                <li>Users</li>
-            </ul>
-            <button className="nav--login_button">Login</button>
-        </nav>)
+    return (<>
+            <nav>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h3 className="nav--logo_text">WheresOurMoney</h3>
+                <ul className="nav--list">
+                    <li className="nav--list_item">
+                        <Link className="nav--list_item_link"
+                              to="/entries">Accounts</Link>
+                    </li>
+                    <li className="nav--list_item">
+                        <Link className="nav--list_item_link"
+                              to="/categories">Categories</Link>
+                    </li>
+                    <li className="nav--list_item">
+                        <Link className="nav--list_item_link"
+                              to="/users">Users</Link>
+                    </li>
+                </ul>
+                <button className="nav--login_button">Login</button>
+            </nav>
+        </>
+    )
 }
